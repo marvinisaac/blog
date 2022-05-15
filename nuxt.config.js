b933c671fa552e3b409b24ce4ea79a000a0075a3
Marvin <4396841+marvinisaac@users.noreply.github.com>
2022-05-15 16:55:46 +0800

    :pencil2: Customize site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blog',
+    title: 'Marvin\'s Blog',
     htmlAttrs: {
       lang: 'en'
     },
